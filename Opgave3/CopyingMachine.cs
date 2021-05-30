2f22d7b4119144a5ff6270c35a1f3ba68aeba3ea
frank <f.dersjant@inholland.nl>
2021-05-30 20:16:12 +0200

    Singleton

---
+++ Opgave3/CopyingMachine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Opgave3
+{
+    public class CopyingMachine
+    {
+        private int totalNumberOfCopies;
+        private static CopyingMachine uniqueInstance;
+        public int TotalNumberOfCopies
+        {
+            get { return totalNumberOfCopies; }
+        }
+        private CopyingMachine()
+        {
+            totalNumberOfCopies = 0;
+        }
+        public static CopyingMachine GetUniqueInstance()
+        {
+            if (uniqueInstance == null)
+            {
+                uniqueInstance = new CopyingMachine();
+            }
+            return uniqueInstance;
+        }
+        public void Copy(int nrOfCopies)
+        {
+            Console.WriteLine("copying, {0}x", nrOfCopies);
+            totalNumberOfCopies += nrOfCopies;
+            Console.WriteLine("total number of copies: {0}x", totalNumberOfCopies);
+        }
+    }
+}
---
+++ Opgave3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Opgave3
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            Program myProgram = new Program();
+            myProgram.Start();
+        }
+        void Start()
+        {
+            CopyingMachine machine1 = CopyingMachine.GetUniqueInstance();
+            CopyingMachine machine2 = CopyingMachine.GetUniqueInstance();
+            Console.WriteLine("copying with 'machine 1'");
+            machine1.Copy(10);
+            machine1.Copy(23);
+            Console.WriteLine();
+            Console.WriteLine("copying with 'machine 2'");
+            machine2.Copy(40);
+            Console.ReadKey();
+        }
+    }
+}
